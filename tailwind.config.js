730d997805ed69d5b0e301ce2d46d628fc6634f1
Heaven Aldrico <aldrico.ffs@gmail.com>
2023-05-21 14:02:47 +0700

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}', './dist/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        background: '#202124'
+      }
+    }
+  },
+  plugins: []
+};
